b3a89b6c22a24d2f476f4645cfec838b54a86671
agustina-biscayart-movile <agustina.biscayart@movile.com>
2016-04-26 00:19:18 -0300

    adding Ej3

--- simusched/tasks.cpp
+++ simusched/tasks.cpp
@@ -1,5 +1,8 @@
 #include "tasks.h"
 
+#define MUST_EXECUTE_CPU 1
+#define MUST_EXECUTE_IO 0
+
 using namespace std;
 
 void TaskCPU(int pid, vector<int> params) { // params: n
@@ -18,7 +21,7 @@ void TaskAlterno(int pid, vector<int> params) { // params: ms_pid, ms_io, ms_pid
 	}
 }
 
-
+// Ej1.
 void TaskConsola(int pid, vector<int> params){
 	/**
 	 * Cantidad de llamadas bloqueantes
@@ -48,6 +51,53 @@ void TaskConsola(int pid, vector<int> params){
 	}
 }
 
+
+// Ej3. (No entiendo bien la aclaración entre () de los tiempos. Revisar.)
+void TaskBatch(int pid, vector<int> params) { // params: total_cpu, cant_bloqueos
+	/**
+	 * Guardo los parametros de entrada que vienen desde la
+	 * consola, los definimos como total_cpu que representa
+	 * la cantidad de ciclos de CPU a ejecutar y cant_bloqueos
+	 * que representa la cantidad de bloqueos que va a tener
+	 * el sistema.
+	 */
+	int total_cpu = params[0];
+	int cant_bloqueos = params[1];
+	/**
+	 * Generamos un vector con la cantidad de ciclos que va a
+	 * tener la CPU, todas las posiciones son rellenadas con
+	 * el flag MUST_EXECUTE_CPU. Es decir, en donde el valor
+	 * del vector es MUST_EXECUTE_CPU, el sistema debera
+	 * ejecutar un ciclo de CPU.
+	 */
+	std::vector<int> ciclos(total_cpu, MUST_EXECUTE_CPU);
+	/**
+	 * Ahora llenamos las primeras #cant_bloqueos posiciones
+	 * con el flag MUST_EXECUTE_IO. Es decir, en donde el valor
+	 * del vector sea MUST_EXECUTE_IO, debe ejecutar 1 ciclo
+	 * de I/O
+	 */
+	std::fill(ciclos.begin(), ciclos.begin() + cant_bloqueos, MUST_EXECUTE_IO);
+	/**
+	 * Hasta este punto realiza a medias lo solcitado por el
+	 * enunciado, ya que siempre van a ser las primeras llamadas
+	 * las bloqueantes y necesitamos que sean pseudo-aleatorio.
+	 */
+	srand(time(NULL) * pid);
+	std::random_shuffle(ciclos.begin(), ciclos.end()); //, random);
+	/**
+	 * Recorremos el vector y vamos haciendo una a una la ejecucion
+	 */
+	for (int i = 0; i < total_cpu; i++) {
+		if (ciclos[i] == MUST_EXECUTE_CPU){
+			uso_CPU(pid, 1);
+		} else {
+			//TODO: ver si solo esta llamada tiene que tardar 2 ciclos.
+			uso_IO(pid, 2);
+		}
+	}
+
+}
 
 void tasks_init(void) {
 	/* Todos los tipos de tareas se deben registrar acá para poder ser usadas.
