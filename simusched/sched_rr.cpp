c4eea6cf839e4e601a506ed65df44c2b8236b5d6
agustina-biscayart-movile <agustina.biscayart@movile.com>
2016-04-26 00:31:52 -0300

    ejercicio 4

--- simusched/sched_rr.cpp
+++ simusched/sched_rr.cpp
@@ -6,10 +6,13 @@
 
 using namespace std;
 
+// Ej4.
 SchedRR::SchedRR(vector<int> argn) {
 	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	for (int i = 0; i < argn[1]; i++) {
+		quantum.push_back(argn[i+2]);
+		core.push_back(argn[i+2]);
+	}
 }
 
 SchedRR::~SchedRR() {
@@ -18,18 +21,88 @@ SchedRR::~SchedRR() {
 
 
 void SchedRR::load(int pid) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	process.push(pid);
 }
 
 void SchedRR::unblock(int pid) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	process.push(pid);
 }
 
 int SchedRR::tick(int cpu, const enum Motivo m) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	/**
+	 * Si el proceso ha finalizado, verifica si en
+	 * la cola de procesos restantes hay otra tarea
+	 * a ejecutar, si no hay otra, retorna la tarea
+	 * IDLE
+	 */
+	if (m == EXIT) {
+		if (process.empty()) {
+			return IDLE_TASK;
+		} else {
+			reset(cpu);
+			return next(cpu);
+		}
+	}
+
+	/**
+	 * Cuando se produce un TICK, puede ser
+	 * porque el IDLE se esta ejecutando o
+	 * se esta ejecutando un proceso.
+	 *
+	 * Si proceso proceso es de tipo IDLE
+	 * verifica si hay otro proceso enconlado
+	 * sino, sigue ejecutando IDLE.
+	 *
+	 * En caso que sea otro proceso, verifica
+	 * si tiene quantum para seguir ejecutandose
+	 * en caso que no lo tenga, verifica si hay
+	 * otro proceso pendiente a ser ejecutado, de
+	 * ser asi, se cambia de proceso, sino, se sigue
+	 * ejecutando por un quantum mas.
+	 *
+	 */
+	if (m == TICK) {
+		if (current_pid(cpu) == IDLE_TASK) {
+			if (process.empty()) {
+				return IDLE_TASK;
+			} else {
+				reset(cpu);
+				return next(cpu);
+			}
+		} else {
+			if (core[cpu] == 0) {
+				if (process.empty()) {
+					reset(cpu);
+					return current_pid(cpu);
+				} else {
+					process.push(current_pid(cpu));
+					reset(cpu);
+					return next(cpu);
+				}
+			} else {
+				core[cpu]--;
+				return current_pid(cpu);
+			}
+		}
+	}
+
+	if (m == BLOCK) {
+		if (process.empty()) {
+			return IDLE_TASK;
+		} else {
+			reset(cpu);
+			return next(cpu);
+		}
+	}
+	return IDLE_TASK;
 }
 
-//<<<REMOVE>>>
+int SchedRR::next(int cpu) {
+	int next_pid = process.front();
+	process.pop();
+	return next_pid;;
+}
+
+void SchedRR::reset(int cpu) {
+	core[cpu] = quantum[cpu];
+}
--- simusched/sched_rr.h
+++ simusched/sched_rr.h
@@ -3,7 +3,15 @@
 
 #include <vector>
 #include <queue>
+#include <iostream>
+#include <list>
 #include "basesched.h"
+
+using namespace std;
+
+typedef int pid;
+
+typedef int cpu;
 
 class SchedRR : public SchedBase {
 	public:
@@ -14,6 +22,36 @@ class SchedRR : public SchedBase {
 		virtual int tick(int cpu, const enum Motivo m);
 
 	private:
+		/**
+		 * Cola para todos los procesos que van llegando
+		 * al scheduler, es una unica cola en comun para
+		 * todos los proceso.
+		 */
+		std::queue<pid> process;
+		/**
+		 * Quantum que tiene el scheduler para cada uno
+		 * de los procesadores que tiene el sistema, el
+		 * indice del operador [] representa el numero
+		 * de procesador con indice en 0.
+		 */
+		std::vector<cpu> quantum;
+		/**
+		 * Representa el quantum restante que tiene cada
+		 * uno de los procesadores que tiene el sistema,
+		 * el indice del operador [] representa el numero
+		 * de procesador con indice en 0.
+		 */
+		std::vector<cpu> core;
+		/**
+		 * Retorna el proximo proceso de la cola, requiere
+		 * que la cola no se encuentre vacia.
+		 */
+		int next(int cpu);
+		/**
+		 * Restablece los valores del quantum para un
+		 * determinado CPU.
+		 */
+		void reset(int cpu);
 };
 
 #endif
