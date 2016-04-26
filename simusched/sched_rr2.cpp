f66ca61af9edcfe25f9951354f4c561483171753
agustina-biscayart-movile <agustina.biscayart@movile.com>
2016-04-26 01:10:44 -0300

    ejercicio 8 - implementacion

--- simusched/sched_rr2.cpp
+++ simusched/sched_rr2.cpp
@@ -1,38 +1,153 @@
-#include <vector>
-#include <queue>
 #include "sched_rr2.h"
-#include "basesched.h"
-#include <iostream>
 
 using namespace std;
 
+struct operation {
+	int operator()(std::queue<int> &e1, std::list<int> &e2){
+		return e1.size() + e2.size();
+	}
+} add;
+
 SchedRR2::SchedRR2(vector<int> argn) {
 	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	for (int i = 0; i < argn[1]; i++) {
+		std::list<pid> l;
+		std::queue<pid> q;
+
+		process.push_back(q);
+		blocked.push_back(l);
+		active.push_back(0);
+
+		quantum.push_back(argn[i + 2]);
+		core.push_back(argn[i + 2]);
+	}
 }
 
 SchedRR2::~SchedRR2() {
 
 }
 
-
 void SchedRR2::load(int pid) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	/**
+	 * Busco el proceso que tiene menor cantidad de activo totales,
+	 * la suma la obtiene de RUNNING + BLOCKED + READY
+	 */
+	std::vector<int> candidate(blocked.size(), 0);
+	transform(process.begin(), process.end(), blocked.begin(), candidate.begin(), add);
+	transform(candidate.begin(), candidate.end(), active.begin(), candidate.begin(), std::plus<int>());
+	
+	/**
+	* Hasta aca tenemos la suma de las 3 colas. Ahora buscamos el minimo usando std de c.
+	* Primero calculamos el iterador que apunta al minimo y luego calculamos el indice del minimo
+	* calculando la distancia entre el inicio del vector y el iterador. 
+	*/
+	std::vector<int>::iterator minium = std::min_element(candidate.begin(), candidate.end());
+	int cpu = std::distance(candidate.begin(), minium);
+
+	process[cpu].push(pid);
 }
 
 void SchedRR2::unblock(int pid) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	std::list<int>::iterator it;
+	/**
+	 * Busco el pid en la cola que se encuentra bloqueado
+	 * y lo migro hacia los que estan para ser ejecutados
+	 */
+	for(unsigned int cpu = 0; cpu < blocked.size(); cpu ++){
+		it = find(blocked[cpu].begin(), blocked[cpu].end(), pid);
+		if(it != blocked[cpu].end()){
+			blocked[cpu].remove(pid);
+			process[cpu].push(pid);
+			return;
+		}
+	}
 }
 
 int SchedRR2::tick(int cpu, const enum Motivo m) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	/**
+	 * Si el proceso ha finalizado, verifica si en
+	 * la cola de procesos restantes hay otra tarea
+	 * a ejecutar, si no hay otra, retorna la tarea
+	 * IDLE
+	 */
+	if (m == EXIT) {
+		if (process[cpu].empty()) {
+			active[cpu] = IDLE;
+			return IDLE_TASK;
+		} else {
+			active[cpu] = BUSY;
+			reset(cpu);
+			return next(cpu);
+		}
+	}
+
+	/**
+	 * Cuando se produce un TICK, puede ser
+	 * porque el IDLE se esta ejecutando o
+	 * se esta ejecutando un proceso.
+	 *
+	 * Si proceso proceso es de tipo IDLE
+	 * verifica si hay otro proceso enconlado
+	 * sino, sigue ejecutando IDLE.
+	 *
+	 * En caso que sea otro proceso, verifica
+	 * si tiene quantum para seguir ejecutandose
+	 * en caso que no lo tenga, verifica si hay
+	 * otro proceso pendiente a ser ejecutado, de
+	 * ser asi, se cambia de proceso, sino, se sigue
+	 * ejecutando por un quantum mas.
+	 *
+	 */
+	if (m == TICK) {
+		if (current_pid(cpu) == IDLE_TASK) {
+			if (process[cpu].empty()) {
+				active[cpu] = IDLE;
+				return IDLE_TASK;
+			} else {
+				active[cpu] = BUSY;
+				reset(cpu);
+				return next(cpu);
+			}
+		} else {
+			if (core[cpu] == 0) {
+				if (process[cpu].empty()) {
+					active[cpu] = BUSY;
+					reset(cpu);
+					return current_pid(cpu);
+				} else {
+					active[cpu] = BUSY;
+					process[cpu].push(current_pid(cpu));
+					reset(cpu);
+					return next(cpu);
+				}
+			} else {
+				active[cpu] = BUSY;
+				core[cpu]--;
+				return current_pid(cpu);
+			}
+		}
+	}
+
+	if (m == BLOCK) {
+		blocked[cpu].push_back(current_pid(cpu));
+		if (process[cpu].empty()) {
+			active[cpu] = IDLE;
+			return IDLE_TASK;
+		} else {
+			active[cpu] = BUSY;
+			reset(cpu);
+			return next(cpu);
+		}
+	}
+	return IDLE_TASK;
 }
 
 int SchedRR2::next(int cpu) {
-//<<<REMOVE>>>
-//<<<REMOVE END>>>
+	int next_pid = process[cpu].front();
+	process[cpu].pop();
+	return next_pid;
+}
+
+void SchedRR2::reset(int cpu) {
+	core[cpu] = quantum[cpu];
 }
--- simusched/sched_rr2.h
+++ simusched/sched_rr2.h
@@ -1,9 +1,40 @@
 #ifndef __SCHED_RR2__
 #define __SCHED_RR2__
 
+#include <iostream>
 #include <vector>
 #include <queue>
+#include <list>
+#include <algorithm>
 #include "basesched.h"
+
+/**
+ * Indica que el core se encuentra con una
+ * tarea en estado RUNNING.
+ */
+#define BUSY 1
+/**
+ * Indica que el core no se encuentra con una
+ * tarea en estado RUNNING
+ */
+#define IDLE 0
+
+using namespace std;
+
+/**
+ * Defino tipo de dato PID.
+ * Solo agrega mas legibilidad
+ * al codigo. Representa el pid
+ * que viene como numero entero.
+ */
+typedef int pid;
+/**
+ * Defino tipo de dato CPU.
+ * Solo agrega mas legibilidad
+ * al codigo. Representa el core
+ * den una cpu.
+ */
+typedef int cpu;
 
 class SchedRR2 : public SchedBase {
 	public:
@@ -14,7 +45,49 @@ class SchedRR2 : public SchedBase {
 		virtual int tick(int cpu, const enum Motivo m);
 
 	private:
+		/**
+		 * Cola para todos los procesos que van llegando
+		 * al scheduler, es una unica cola en comun para
+		 * todos los proceso.
+		 */
+		std::vector<std::queue<pid> > process;
+		/*
+		 * Contiene los procesos que se encuentran bloqueados
+		 * por llamadas I/O.
+		 */
+		std::vector<std::list<pid> > blocked;
+
+		/**
+		 * Quantum que tiene el scheduler para cada uno
+		 * de los procesadores que tiene el sistema, el
+		 * indice del operador [] representa el numero
+		 * de procesador con indice en 0.
+		 */
+		std::vector<cpu> quantum;
+		/**
+		 * Representa el quantum restante que tiene cada
+		 * uno de los procesadores que tiene el sistema,
+		 * el indice del operador [] representa el numero
+		 * de procesador con indice en 0.
+		 */
+		std::vector<cpu> core;
+		/**
+		 * Representa si el procesador se encuentra con
+		 * un proceso activo. Si el valor es 1, indica
+		 * que el cpu esta con un proceso activo, si el
+		 * valor es 0 representa que el proceso esta
+		 * ejecutando idle
+		 */
+		std::vector<cpu> active;
+		/**
+		 * Sin implementacion.
+		 */
 		int next(int cpu);
+		/**
+		 * Restablece los valores del quantum para un
+		 * determinado CPU.
+		 */
+		void reset(int cpu);
 };
 
 #endif
