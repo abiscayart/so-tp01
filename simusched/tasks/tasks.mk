3c6bbedde35a8e43659e24cfc2795b9ba0ebb184
agustina-biscayart-movile <agustina.biscayart@movile.com>
2016-04-26 02:39:52 -0300

    fix lote de tareas ejercicio 2 + make

--- simusched/tasks/tasks.mk
+++ simusched/tasks/tasks.mk
@@ -13,7 +13,8 @@ ejercicio02: all
 	@echo '[INFO] EJERCICIO 02'
 	@echo '[INFO] ------------------------------------------------------------------------'
 	@echo -e '\033[0m'
-	simusched tasks/tasks_002.tsk 1 1 4 SchedFCFS > tasks/output/tasks_002.out 
+	
+	simusched tasks/tasks_002.tsk 1 5 1 SchedFCFS > tasks/output/tasks_002.out 
 	cat tasks/output/tasks_002.out | graphsched.py   > tasks/images/tasks_001.png
 	cat tasks/output/tasks_002.out | graph_cores.py  >  /dev/null 2>&1
 	mv out_cores_timeline.png tasks/images/tasks_002.png
@@ -21,7 +22,8 @@ ejercicio02: all
 	mv out_cores_resume.png  tasks/images/tasks_003.png
 	mv out_pids_resume.png   tasks/images/tasks_004.png
 	mv out_pids_timeline.png tasks/images/tasks_005.png
-	simusched tasks/tasks_002.tsk 2 1 4 SchedFCFS > tasks/output/tasks_003.out 
+	
+	simusched tasks/tasks_002.tsk 2 5 1 SchedFCFS > tasks/output/tasks_003.out 
 	cat tasks/output/tasks_003.out | graphsched.py  > tasks/images/tasks_011.png
 	cat tasks/output/tasks_003.out | graph_cores.py >  /dev/null 2>&1
 	mv out_cores_timeline.png tasks/images/tasks_012.png
@@ -29,3 +31,13 @@ ejercicio02: all
 	mv out_cores_resume.png   tasks/images/tasks_013.png
 	mv out_pids_resume.png    tasks/images/tasks_014.png
 	mv out_pids_timeline.png  tasks/images/tasks_015.png
+	
+	simusched tasks/tasks_002.tsk 4 5 1 SchedFCFS > tasks/output/tasks_004.out 
+	cat tasks/output/tasks_004.out | graphsched.py  > tasks/images/tasks_021.png
+	cat tasks/output/tasks_004.out | graph_cores.py >  /dev/null 2>&1
+	mv out_cores_timeline.png tasks/images/tasks_022.png
+	cat tasks/output/tasks_002.out | event_parser.py > /dev/null 2>&1
+	mv out_cores_resume.png   tasks/images/tasks_023.png
+	mv out_pids_resume.png    tasks/images/tasks_024.png
+	mv out_pids_timeline.png  tasks/images/tasks_025.png
+
