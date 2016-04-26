d2121d6e5580e2639210621b7176fd3cdfc0bf34
agustina-biscayart-movile <agustina.biscayart@movile.com>
2016-04-26 00:50:08 -0300

    ejercicio 7 primera parte. Falta probar y graficar

--- simusched/sched_no_mistery.cpp
+++ simusched/sched_no_mistery.cpp
@@ -1,23 +1,102 @@
 #include <vector>
 #include <queue>
+#include <map>
 #include "sched_no_mistery.h"
 #include "basesched.h"
 
 using namespace std;
 
+// Ej7. - primera parte
 SchedNoMistery::SchedNoMistery(vector<int> argn) {
   // MFQ recibe los quantums por parÃ¡metro
+	for (unsigned int i = 0; i < argn.size(); i++) {
+		quantum.push_back(argn[i]-1);
+		queue<pid> entrada;
+		process.push_back(entrada);
+	}
 }
 
+/** El proceso que llega empezara a ejecutar en el primer nivel.*/ 
 void SchedNoMistery::load(int pid) {
+	QuantumTarea.insert(std::pair<int,int>(pid,0));
+	process[0].push(pid);
 }
 
 void SchedNoMistery::unblock(int pid) {
+	process[QuantumTarea[pid]].push(pid);
 }
 
 int SchedNoMistery::tick(int cpu, const enum Motivo m) {
+	/**
+	 * El proseso termino => buscamos el siguiente.
+	 */ 
+	if (m == EXIT) {
+		return next();
+	}
+
+	/**
+	 * Si el proseso es IDLE, seguimos buscando. Si se termino el quantum
+	 * de la tarea actual, se sube el nivel de la tarea actual (si no esta
+	 * en el ultimo nivel) y se vuelve a encolar en el nivel correspondiente
+	 * y se busca el siguiente. Sino se resta en 1 el quantum.
+	 */
+	 if (m == TICK) {
+		if (current_pid(cpu) == IDLE_TASK) {
+				return next();
+
+		} else {
+			if (QuantumActual == 0) { // termino su quantum
+				int cpid=current_pid(cpu);
+					if(QuantumTarea[cpid] < (quantum.size() - 1))
+						QuantumTarea[cpid]++;
+						
+					process[QuantumTarea[cpid]].push(cpid);
+					return next();
+			} else {
+				QuantumActual--;
+				return current_pid(cpu);
+			}
+		}
+	}
+	
+	/**
+	 * Si el proceso se bloquea, =>  se le baja un nivel de quantum y 
+	 * se busca el siguiente.
+	 */
+
+	if (m == BLOCK) {
+			int cpid=current_pid(cpu);
+			if(QuantumTarea[cpid] > 0)
+				QuantumTarea[cpid]--;
+			return next();
+		}
+		
+	return IDLE_TASK;
 }
 
+/**
+ * Buscamos recorremos process desde el primer nivel, fijandonos si hay
+ * algun proceso encolado, de ser asi, ese sera el proseso a ejecutar.
+ * Sino se busca en el siguiente nivel. 
+ * Si recorrimos todos los niveles y no hay ningun proceso encolado 
+ * devolvemos IDLE. Si no devolvemos IDLE => QuantumActual sera la cantidad
+ * del nivel actual.
+ */
+
 int SchedNoMistery::next(void) {
-  // Elijo el nuevo pid
+	int next_pid=IDLE_TASK;
+	
+	for(unsigned int i=0; i<quantum.size(); i++) {
+		if(process[i].empty())
+			continue;
+			
+		next_pid=process[i].front();
+		process[i].pop();
+		break;
+	}
+	
+	if(next_pid!=IDLE_TASK)
+		QuantumActual=quantum[QuantumTarea[next_pid]];
+	
+	return next_pid;
 }
--- simusched/sched_no_mistery.h
+++ simusched/sched_no_mistery.h
@@ -3,7 +3,12 @@
 
 #include <vector>
 #include <queue>
+#include <map>
 #include "basesched.h"
+
+typedef int pid;
+
+typedef int cpu;
 
 class SchedNoMistery : public SchedBase {
   public:
@@ -13,10 +18,35 @@ class SchedNoMistery : public SchedBase {
     virtual int tick(int cpu, const enum Motivo m);
 
   private:
-    std::vector<std::queue<int> > vq;
+
+    /*std::vector<std::queue<int> > vq;
     std::vector<int> def_quantum;
     std::vector<int> unblock_to;
-    int quantum, n, cur_pri;
+    int quantum, n, cur_pri;*/
+
+
+    /**
+     * Vector de Colas para todos los procesos que van llegando
+     * al scheduler, es una unico vector para
+     * todos los proceso. Cada process[i] representa el nivel
+     * de quantum en el que ejecutaran las tareas encoladas.
+     */
+     std::vector< std::queue<pid> >process;
+    /**
+     * Vector de Quantums pasados por parametro.
+     */
+    std::vector<cpu> quantum;
+    /**diccionario que vincula un proceso con su quantum
+     * QuantumTarea[pid] devuelve la posicion en el vector
+     * quantum[] para la tarea pid.
+     */
+    std::map<int,unsigned int> QuantumTarea;
+    //
+    int QuantumActual;
+    /**
+     * Retorna el proximo proceso de la cola, requiere
+     * que la cola no se encuentre vacia.
+     */
 
     int next(void);
 };
