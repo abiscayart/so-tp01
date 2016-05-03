068f63273eede7cf34523d6518f14a1d80139e2b
agustina-biscayart-movile <agustina.biscayart@movile.com>
2016-05-03 01:51:22 -0300

    update images ejercicio 7

--- simusched/ej7/mist.mk
+++ simusched/ej7/mist.mk
@@ -4,9 +4,10 @@ ejercicio7: all
 	@echo -e '\033[0;32m'
 	@echo '[INFO] Generating images for ejercicio 7...'
 	@echo -e '\033[0m'
-	simusched ej7/mist.tsk 1 2 0 SchedMistery 5 15 10 4 2 | ./graphsched.py > ej7/images/mist1.png
-	simusched ej7/mist2.tsk 1 2 0 SchedMistery 5 15 10 4 2 | ./graphsched.py > ej7/images/mist2.png
-	simusched ej7/mist3.tsk 1 2 0 SchedMistery 5 15 10 4 2 | ./graphsched.py > ej7/images/mist3.png
-	simusched ej7/mist.tsk 1 2 0 SchedNoMistery 5 15 10 4 2 | ./graphsched.py > ej7/images/mist11.png
-	simusched ej7/mist2.tsk 1 2 0 SchedNoMistery 5 15 10 4 2 | ./graphsched.py > ej7/images/mist22.png
-	simusched ej7/mist3.tsk 1 2 0 SchedNoMistery 5 15 10 4 2 | graphsched.py > ej7/images/mist33.png
+	simusched ej7/mist.tsk 1 0 0 SchedMistery 10 7 5 3 | ./graphsched.py > ej7/images/mist1.png
+	simusched ej7/mist2.tsk 1 0 0 SchedMistery 20 5 15 7 23 35 | ./graphsched.py > ej7/images/mist2.png
+	simusched ej7/mist3.tsk 1 0 0 SchedMistery 5 10 15 20 25 30 35 | ./graphsched.py > ej7/images/mist3.png
+	
+	simusched ej7/mist.tsk 1 0 0 SchedNoMistery 10 7 5 3 | ./graphsched.py > ej7/images/no-mist1.png
+	simusched ej7/mist2.tsk 1 0 0 SchedNoMistery 20 5 15 7 23 35 | ./graphsched.py > ej7/images/no-mist2.png
+	simusched ej7/mist3.tsk 1 0 0 SchedNoMistery 5 10 15 20 25 30 35 | graphsched.py > ej7/images/no-mist3.png
